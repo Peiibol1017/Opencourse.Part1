c57a60fd9a2676accd4c9f882c8e02df57da25c5
pablo <peiibol1017@gmail.com>
2025-02-21 11:40:39 +0100

    part 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,12 +5,27 @@ const Header = (title) =>{
     </>
   )
 }
+const Part1 = (parts1) => {
+  return(
+    <p>{parts1.p1} have {parts1.e1} exercises</p>
+  )
+}
+const Part2 = (parts2) => {
+  return(
+<p>{parts2.p2} have {parts2.e2} exercises</p>
+  )
+}
+const Part3 = (parts3) => {
+  return(
+<p>{parts3.p3} have {parts3.e3} exercises</p>
+  )
+}
 const Content = (contents) => {
   return (
   <div>
-    <p>{contents.p1}{contents.e1}</p>
-    <p>{contents.p2}{contents.e2}</p>
-    <p>{contents.p3}{contents.e3}</p>
+    <Part1 p1={contents.p1} e1={contents.e1}/>
+    <Part2 p2={contents.p2} e2={contents.e2}/>
+    <Part3 p3={contents.p3} e3={contents.e3}/>
   </div>
   )
 }
